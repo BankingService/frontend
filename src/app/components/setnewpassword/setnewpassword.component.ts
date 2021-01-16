2a96d3ebaa0a442db068add2c17f8c7ebf675eef
nishitajain99 <jainnishita99@gmail.com>
2021-01-16 22:30:09 +0530

    Set new password css

--- src/app/components/setnewpassword/setnewpassword.component.ts
+++ src/app/components/setnewpassword/setnewpassword.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 
@@ -14,14 +14,70 @@ export class SetnewpasswordComponent implements OnInit {
   loginPassword:String
   transactionPassword:String
 
+  error_messages = {
+    'loginpassword': [
+      { type: 'required', message: 'Password is required.' },
+        { type: 'minlength', message: 'Password length too small' },
+        { type: 'maxlength', message: 'Exceeds password length limit' },
+        { type: 'pattern', message:'Password must consist one special character,one alphabet and one numeric'}
+    ],
+    'confloginpassword': [
+        { type: 'required', message: 'Password is required.' },
+        { type: 'minlength', message: 'Password length too small' },
+        { type: 'maxlength', message: 'Exceeds password length limit' },
+        { type: 'pattern', message:'Password must consist one special character,one alphabet and one numeric'}
+    ],
+    'transactionpassword': [
+      { type: 'required', message: 'Password is required.' },
+        { type: 'minlength', message: 'Password length too small' },
+        { type: 'maxlength', message: 'Exceeds password length limit' },
+        { type: 'pattern', message:'Password must consist one special character,one alphabet and one numeric'}
+    ],
+    'conftransactionpassword': [
+        { type: 'required', message: 'Password is required.' },
+        { type: 'minlength', message: 'Password length too small' },
+        { type: 'maxlength', message: 'Exceeds password length limit' },
+        { type: 'pattern', message:'Password must consist one special character,one alphabet and one numeric'}
+    ],
+    
+  }  
+
   constructor(private http:UserService,private router:Router) { }
 
   ngOnInit() {
     this.form = new FormGroup({
-      loginpassword: new FormControl(),
-      confloginpassword: new FormControl(),
-      transactionpassword: new FormControl(),
-      conftransactionpassword: new FormControl()
+      loginpassword: new FormControl(
+        '', Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(15),
+          Validators.pattern('(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:\{\\\}\\\[\\\]\\\|\\\+\\\-\\\=\\\_\\\)\\\(\\\)\\\`\\\/\\\\\\]])[A-Za-z0-9\d$@].{7,}')  
+        ])
+      ),
+      confloginpassword: new FormControl(
+        '', Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(15),
+          Validators.pattern('(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:\{\\\}\\\[\\\]\\\|\\\+\\\-\\\=\\\_\\\)\\\(\\\)\\\`\\\/\\\\\\]])[A-Za-z0-9\d$@].{7,}')          
+        ])
+      ),
+      transactionpassword: new FormControl(
+        '', Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(15),
+          Validators.pattern('(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:\{\\\}\\\[\\\]\\\|\\\+\\\-\\\=\\\_\\\)\\\(\\\)\\\`\\\/\\\\\\]])[A-Za-z0-9\d$@].{7,}')         
+        ])
+      ),
+      conftransactionpassword: new FormControl(
+        '', Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(15),
+          Validators.pattern('(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>"\'\\;:\{\\\}\\\[\\\]\\\|\\\+\\\-\\\=\\\_\\\)\\\(\\\)\\\`\\\/\\\\\\]])[A-Za-z0-9\d$@].{7,}')          
+        ])
+      )
    });
   }
 
